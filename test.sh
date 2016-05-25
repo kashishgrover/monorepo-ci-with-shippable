cde190954cea6c7bf7f0773d3ca7862087181663
Kashish Grover <kashishgrover95@gmail.com>
2016-05-25 16:39:43 +0530

    Message

--- test.sh
+++ test.sh
@@ -1,8 +1,24 @@
 #finding the list of folders that have changed with two given commit id's 
 
-read com1
-read com2
-
-readarray -t array <<< "$(git diff --name-only $com1 $com2)"
+#read com1
+#read com2
+#readarray -t array <<< "$(git diff --name-only $com1 $com2)"
+readarray -t array <<< "$(git diff --name-only HEAD HEAD~1)"
 
 printf -- "%s\n" "${array[@]}"
+
+for each in "${array[@]}"
+do
+	if [ -d "$each" ] 
+	then 
+		echo "Directory!"
+		echo "------"
+		pushd /home/kashish/Desktop/SampleNodeJS_TwoApps/"$each"/	
+		echo "------"	
+		ls
+		echo "------"
+		docker build -t kashishgrover/${each,,}build:latest .
+		echo "------"
+		popd
+	fi
+done
