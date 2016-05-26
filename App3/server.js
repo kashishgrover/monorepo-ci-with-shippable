8d3089b617289610c5e6f133a85fc5bf6fc0bc68
Kashish Grover <kashishgrover95@gmail.com>
2016-05-26 12:12:47 +0530

    Update server.js

--- App3/server.js
+++ App3/server.js
@@ -8,7 +8,7 @@ http.createServer(function (request, response) {
    response.writeHead(200, {'Content-Type': 'text/plain'});
    
    // Send the response body as "Hello World"
-   response.end('Hello World! This is a test! Are you going to Scarborough Fair?? Lol! Hodor!');
+   response.end('asdfasdf');
 }).listen(8081);
 
 // Console will print the message
