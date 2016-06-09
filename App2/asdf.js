ea22491791ca25c4c3fba515a32371e48f8e381e
Kashish Grover <kashishgrover95@gmail.com>
2016-06-09 17:37:33 +0530

    Update asdf.js

--- App2/asdf.js
+++ App2/asdf.js
@@ -1,7 +1,1 @@
-console.log("Hold the door!");
-console.log("Hold door!");
-console.log("Holdoor!");
-console.log("HODOR!");
-
-console.log("Gary said Mau");
-console.log("Patrick and Spongebob catch jellyfishes")
+console.log("Hello :D");
