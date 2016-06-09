bdbbe32d484dc856fec2f09bf1db2fcafbed13da
Kashish Grover <kashishgrover95@gmail.com>
2016-06-09 15:34:21 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,73 +1,64 @@
-#!/bin/bash
-
-#Shippable's Environment Variable which gives the current Commit ID
-echo "COMMIT = $COMMIT"
-
 #Put your image name over here
 export IMAGE_NAME=kashishgrover/samplenodejstwoapps
 
-#Shippable's Environment Variable which gives the range between 
-#the last successful Commit ID and the  current Commit ID
-#in the form <COMMIT_ID_1>...<COMMIT_ID_2>
-echo "SHIPPABLE_COMMIT_RANGE = $SHIPPABLE_COMMIT_RANGE"
+#SHIPPABLE_COMMIT_RANGE is an Environment Variable which
+#gives the range between the last successful Commit ID and
+#the  current Commit ID as <COMMIT_ID_1>...<COMMIT_ID_2>
 
-#We can extract the previous commit ID using the Internal Field Separator (IFS) as '...'
-COMMITPREV=$(IFS="..." ; set -- $SHIPPABLE_COMMIT_RANGE ; echo $1)
-echo "COMMITPREV = $COMMITPREV"
+detect_changed_files_and_folders() {
+  echo "Detecting Changes For This Build"
+  languages=`git diff --name-only $SHIPPABLE_COMMIT_RANGE | sort -u | awk 'BEGIN {FS="/"} {print $1}' | uniq`
+  for language in $languages
+  do
+    detect_changed_folders $language
+  done
+}
 
-#Store the list of files which have changed between the two commits within an array
-readarray -t array <<< "$(git diff --name-only $COMMIT $COMMITPREV)" 
+detect_changed_folders() {
+  folders=`git diff --name-only $SHIPPABLE_COMMIT_RANGE | sort -u | grep $1 | awk 'BEGIN {FS="/"} {print $2}' | uniq`
+  push_all_images=false
+  for folder in $folders
+  do
+    if [ "$folder" == '_global' ]; then
+      echo "pushing all images"
+      push_all_images=true
+      break
+    fi
+  done
 
-printf -- "%s\n" "${array[@]}"
-printf "\n"
+  if [ "$push_all_images" == true ]; then
+    cd $1
+    export changed_components=`ls -d */ | sed 's/.$//'`
+    cd ..
+  else
+    export changed_components=$folders
+  fi
+}
 
-# A loop which is used to extract the directory names
-# of the files which have changed between the two commits.
-# For example, assume that the files AppFolder/foobar.js and
-# shippable.yml in the project's main folder have changed.
-# The array will consist of 'AppFolder' and 'shippable.yml'
-i=0
-for each in "${array[@]}"									
-do															
-	array[$i]=$(IFS="/" ; set -- $each ; echo $1)			
-	echo ${array[$i]}										
-	i=`expr $i + 1`											
-done
+tag_and_push_changed_components() {
+  for component in $changed_components
+  do
+    if [ "$component" != '_global' ] && [ "$component" != 'node_modules' ]; then
+      tag_and_push_image $component
+    fi
+  done
+}
 
-#Now, we make sure that the folder names don't repeat and store the final unique words in anoher array
-sorted_unique_first_words=$(echo "${array[@]}" | tr ' ' '\n' | sort -u | tr '\n' ' ')
-IFS=' ' read -a WORD_ARRAY <<< "$sorted_unique_first_words"
+tag_and_push_image() {
+  if [[ -z "$1" ]]; then
+    return 0
+  else
+    echo "tagging image $1"
+    sudo docker tag -f $IMAGE_NAME:$BRANCH.$SHIPPABLE_BUILD_NUMBER $IMAGE_NAME:$1.$BRANCH.$SHIPPABLE_BUILD_NUMBER
+    echo "pushing image $1"
+    sudo docker push $IMAGE_NAME:$1.$BRANCH.$SHIPPABLE_BUILD_NUMBER
+  fi
+}
 
-#Get current directory
-curdir=`pwd`
 
-#The following FOR loop will check if a given word in the WORD_ARRAY is a directory or a file.
-#	If it is a directory, push the repository with the tag name as the name of that particular directory
-#	If it is a file, it means that the file has to exist in the main project folder. In this case, push the whole repository.
-
-#Make sure that everything is in lowercase
-
-for each in "${WORD_ARRAY[@]}"
-do
-	if [ -d $each ]
-	then 
-		echo "*IS A DIRECTORY***********************************************"
-		pushd $curdir/"$each"/	
-		echo "------------"
-		#${each,,} will convert folder name to lowercase
-		docker build -t $IMAGE_NAME:$BRANCH.${each,,}build .
-		docker commit $SHIPPABLE_CONTAINER_NAME $IMAGE_NAME:$BRANCH.${each,,}build
-		docker push $IMAGE_NAME:$BRANCH.${each,,}build
-		echo "------------"
-		popd
-		echo "**************************************************************"
-	else
-		echo "*IS A FILE****************************************************"
-		docker build -t $IMAGE_NAME:$BRANCH .
-		docker commit $SHIPPABLE_CONTAINER_NAME $IMAGE_NAME:$BRANCH
-		docker push $IMAGE_NAME:$BRANCH
-		echo "**************************************************************"
-	fi
-done
-
-#--------------#
+if [ "$IS_PULL_REQUEST" != true ]; then
+  detect_changed_languages
+  tag_and_push_changed_components
+else
+  echo "skipping because it's a PR"
+fi
