736aa2151c746e1d772610958e83bed38b7ed7b3
Kashish Grover <kashishgrover95@gmail.com>
2016-06-09 17:55:58 +0530

    Update server.js

--- App3/server.js
+++ App3/server.js
@@ -6,7 +6,7 @@ http.createServer(function (request, response) {
    // HTTP Status: 200 : OK
    // Content Type: text/plain
    response.writeHead(200, {'Content-Type': 'text/plain'});
-   
+   console.log('Meow');
    // Send the response body as "Hello World"
    response.end('This is a test! Hello World!');
 }).listen(8081);
