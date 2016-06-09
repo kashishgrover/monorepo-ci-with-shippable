3a7bb8c202d1c54734d9ed9eeae90def695645f5
Kashish Grover <kashishgrover95@gmail.com>
2016-06-09 15:58:53 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,65 +1,55 @@
+#!/bin/bash
+
 #Put your image name over here
 export IMAGE_NAME=kashishgrover/samplenodejstwoapps
 
-#SHIPPABLE_COMMIT_RANGE is an Environment Variable which
-#gives the range between the last successful Commit ID and
-#the  current Commit ID as <COMMIT_ID_1>...<COMMIT_ID_2>
-
 detect_changed_files_and_folders() {
   echo "Detecting Changes For This Build"
+
+  #SHIPPABLE_COMMIT_RANGE is an Environment Variable which
+  #gives the range between the last successful Commit ID and
+  #the  current Commit ID as <COMMIT_ID_1>...<COMMIT_ID_2>
+
   array=`git diff --name-only $SHIPPABLE_COMMIT_RANGE | sort -u | awk 'BEGIN {FS="/"} {print $1}' | uniq`
-  echo $array
+  printf -- "%s\n" "${array[@]}"
+  printf "\n"
   for element in $array
   do
-    detect_changed_folders $element
+    build_and_push_changed_folder $element
+  done
+  for element in $array
+  do
+    build_and_push_main
+    break
   done
 }
 
-detect_changed_folders() {
-  folders=`git diff --name-only $SHIPPABLE_COMMIT_RANGE | sort -u | grep $1 | awk 'BEGIN {FS="/"} {print $2}' | uniq`
-  push_all_images=false
-  for folder in $folders
-  do
-    if [ "$folder" == '_global' ]; then
-      echo "pushing all images"
-      push_all_images=true
-      break
-    fi
-  done
-
-  if [ "$push_all_images" == true ]; then
-    cd $1
-    export changed_components=`ls -d */ | sed 's/.$//'`
-    cd ..
-  else
-    export changed_components=$folders
+build_and_push_changed_folder() {
+  if [ -d $1 ]; then
+    echo "*THIS IS A DIRECTORY******************************************"
+    curdir=`pwd`
+    pushd $curdir/"$1"/
+    #${1,,} will convert folder name to lowercase
+    docker build -t $IMAGE_NAME:$BRANCH.${1,,}build .
+    docker commit $SHIPPABLE_CONTAINER_NAME $IMAGE_NAME:$BRANCH.${1,,}build
+    docker push $IMAGE_NAME:$BRANCH.${1,,}build
+    popd
   fi
 }
 
-tag_and_push_changed_components() {
-  for component in $changed_components
-  do
-    if [ "$component" != '_global' ] && [ "$component" != 'node_modules' ]; then
-      tag_and_push_image $component
-    fi
-  done
-}
-
-tag_and_push_image() {
-  if [[ -z "$1" ]]; then
-    return 0
-  else
-    echo "tagging image $1"
-    sudo docker tag -f $IMAGE_NAME:$BRANCH.$SHIPPABLE_BUILD_NUMBER $IMAGE_NAME:$1.$BRANCH.$SHIPPABLE_BUILD_NUMBER
-    echo "pushing image $1"
-    sudo docker push $IMAGE_NAME:$1.$BRANCH.$SHIPPABLE_BUILD_NUMBER
+build_and_push_main() {
+  if [ -f $1 ]; then
+    echo "*THIS IS A FILE***********************************************"
+    #${1,,} will convert folder name to lowercase
+    docker build -t $IMAGE_NAME:$BRANCH .
+    docker commit $SHIPPABLE_CONTAINER_NAME $IMAGE_NAME:$BRANCH
+    docker push $IMAGE_NAME:$BRANCH
+    popd
   fi
 }
-
 
 if [ "$IS_PULL_REQUEST" != true ]; then
   detect_changed_files_and_folders
-  tag_and_push_changed_components
 else
-  echo "skipping because it's a PR"
+  echo "skipping because it's not a PR"
 fi
